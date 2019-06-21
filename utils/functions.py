d60e70eaa00aa31c780c642255e080c9af3031cb
Andre Fernandes <andre.fernandes.inbox@gmail.com>
2019-06-20 17:02:52 -0700

    setting up project directory structure

---
+++ utils/functions.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+@authors: Andre Fernandes and Miguel Jaime
+
+Functions for Generative Language Model Project
+
+"""
+
+#####################################
+# imports
+#####################################
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Conv2D, Flatten, MaxPool2D, BatchNormalization
+from keras.callbacks import ModelCheckpoint
+from keras import optimizers, backend
+from keras.models import load_model
+import time
+import pickle
+
+#####################################
+# functions
+#####################################
+
+
